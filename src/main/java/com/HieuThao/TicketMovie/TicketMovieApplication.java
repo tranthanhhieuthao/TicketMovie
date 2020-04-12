97fa2fb0ce736063ae352ffd3b2b6c6ebdfbefa5
tranthanhhieuthao <tranthanhhieuthao@gmail.com>
2020-04-12 15:37:10 -0700

    first pj

---
+++ src/main/java/com/HieuThao/TicketMovie/TicketMovieApplication.java
@@ -0,0 +1,13 @@
+package com.HieuThao.TicketMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HieuThao/TicketMovie/TicketMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HieuThao.TicketMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
